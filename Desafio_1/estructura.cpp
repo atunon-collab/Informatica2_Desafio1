0c00182e56e8a1b69f142f9c758aebcdbc10d9fb
a.tunon <a.tunon@udea.edu.co>
2026-09-18 16:32:12 -0500

    Se mejora la eficiencia de "insertarFila" e "insertarColumna", agregando la función "desplazarInsertados" que reubica las fichas existentes dentro del mismo arreglo ya reservado cuando el espacio alcanza, evitando así invocar "copiarConNuevasDimensiones" (y por lo tanto reservar y copiar en un arreglo nuevo) en cada inserción de fila o columna, además se corrige el tipo de dato que era declarado para el parámetro "tipoOperacion", en un principio era "short" pero fue corregido al tipo "operacion

--- Desafio_1/estructura.cpp
+++ Desafio_1/estructura.cpp
@@ -6,7 +6,7 @@
 // Redimensiona el tablero en memoria dinámica según la operación estructural realizada (agregar/eliminar fila o columna).
 // Reubica las fichas existentes ajustando sus coordenadas, libera la memoria del tablero anterior y asigna el nuevo bloque.
 void copiarConNuevasDimensiones(unsigned char *& tablero, unsigned short filasViejas, unsigned short columnasViejas, unsigned short filasNuevas,
-                                unsigned short columnasNuevas, unsigned short filaAfectada, unsigned short columnaAfectada,short tipoOperacion)
+                                unsigned short columnasNuevas, unsigned short filaAfectada, unsigned short columnaAfectada, operacion tipoOperacion)
 {
     unsigned short nuevosBytes = bytesNecesarios(filasNuevas, columnasNuevas);
     // Reserva e inicializa a cero
@@ -95,12 +95,6 @@ void desplazarEliminadas(unsigned char * tablero, unsigned short filasViejas, un
         escribirFicha(tablero, indNuevo, ficha);
     }
 }
-void insertarFila(unsigned char *& tablero, unsigned short & filas, unsigned short columnas, unsigned short filaAfectada) {
-    unsigned short filasNuevas = filas + 1;
-    copiarConNuevasDimensiones(tablero, filas, columnas, filasNuevas, columnas, filaAfectada, 0, agregarFila);
-    filas = filasNuevas; // Actualiza el tamaño por referencia
-    rellenarVacios(tablero, filas, columnas); // Genera fichas para la nueva fila
-}
 
 void eliminarFila(unsigned char *& tablero, unsigned short & filas, unsigned short columnas,
                   unsigned short filaAfectada, unsigned short & bytesReservados)
@@ -122,11 +116,22 @@ void eliminarFila(unsigned char *& tablero, unsigned short & filas, unsigned sho
     filas = filasNuevas; // Actualiza tamaño
 }
 
-void insertarColumna(unsigned char *& tablero, unsigned short filas, unsigned short & columnas, unsigned short columnaAfectada) {
-    unsigned short columnasNuevas = columnas + 1;
-    copiarConNuevasDimensiones(tablero, filas, columnas, filas, columnasNuevas, 0, columnaAfectada, agregarColumna);
-    columnas = columnasNuevas; // Actualiza el tamaño por referencia
-    rellenarVacios(tablero, filas, columnas); // Genera fichas para la nueva columna
+void insertarFila(unsigned char *& tablero, unsigned short & filas, unsigned short columnas,
+                  unsigned short filaAfectada, unsigned short & bytesReservados)
+{
+    unsigned short filasNuevas = filas + 1;
+    unsigned short bytesNecesariosNuevos = bytesNecesarios(filasNuevas, columnas);
+
+    if (bytesNecesariosNuevos <= bytesReservados) {
+        // Ya hay espacio: desplazamos dentro del mismo bloque, sin pedir memoria nueva
+        desplazarInsertados(tablero, filas, columnas, filasNuevas, columnas, filaAfectada, 0, agregarFila);
+    } else {
+        copiarConNuevasDimensiones(tablero, filas, columnas, filasNuevas, columnas, filaAfectada, 0, agregarFila);
+        bytesReservados = bytesNecesariosNuevos; // actualiza capacidad solo cuando de verdad se reserva
+    }
+
+    filas = filasNuevas;
+    rellenarVacios(tablero, filas, columnas); // llena la fila nueva con fichas aleatorias
 }
 
 void eliminarColumna(unsigned char *& tablero, unsigned short filas, unsigned short & columnas,
@@ -148,7 +153,64 @@ void eliminarColumna(unsigned char *& tablero, unsigned short filas, unsigned sh
     columnas = columnasNuevas; // Actualizamos tamaño
 }
 
+void insertarColumna(unsigned char *& tablero, unsigned short filas, unsigned short & columnas,
+                     unsigned short columnaAfectada, unsigned short & bytesReservados)
+{
+    unsigned short columnasNuevas = columnas + 1;
+    unsigned short bytesNecesariosNuevos = bytesNecesarios(filas, columnasNuevas);
+
+    if (bytesNecesariosNuevos <= bytesReservados) {
+        desplazarInsertados(tablero, filas, columnas, filas, columnasNuevas, 0, columnaAfectada, agregarColumna);
+    } else {
+        copiarConNuevasDimensiones(tablero, filas, columnas, filas, columnasNuevas, 0, columnaAfectada, agregarColumna);
+        bytesReservados = bytesNecesariosNuevos;
+    }
+
+    columnas = columnasNuevas;
+    rellenarVacios(tablero, filas, columnas);
+}
+
 // Evalúa si el tamaño útil ocupado ha caído por debajo del 65% del tamaño reservado
 bool requiereEncogimiento(unsigned short bytesNecesarios, unsigned short bytesReservados) {
     return (bytesNecesarios * 100) < (bytesReservados * 65);
+}
+
+// Abre espacio para una fila o columna nueva dentro del mismo bloque de memoria ya reservado,
+// desplazando las fichas existentes (de atras hacia adelante, para no sobrescribir datos
+// que aun no se han leido), y deja en 0 las celdas de la fila/columna insertadas
+void desplazarInsertados(unsigned char * tablero, unsigned short filasViejas, unsigned short columnasViejas,
+                         unsigned short filasNuevas, unsigned short columnasNuevas,
+                         unsigned short filaAfectada, unsigned short columnaAfectada, short tipoOperacion)
+{
+    unsigned short totalNuevas = filasNuevas * columnasNuevas;
+
+    // Recorre de la ultima posicion a la primera (idiom seguro con unsigned short)
+    for (unsigned short indNuevo = totalNuevas; indNuevo-- > 0; ) {
+        unsigned short filNueva = indNuevo / columnasNuevas;
+        unsigned short colNueva = indNuevo % columnasNuevas;
+
+        short filVieja = filNueva;
+        short colVieja = colNueva;
+        bool esNueva = false; // celda que pertenece a la fila/columna recien insertada
+
+        switch (tipoOperacion) {
+        case agregarFila:
+            if (filNueva == filaAfectada) esNueva = true;
+            else if (filNueva > filaAfectada) filVieja = filNueva - 1;
+            break;
+
+        case agregarColumna:
+            if (colNueva == columnaAfectada) esNueva = true;
+            else if (colNueva > columnaAfectada) colVieja = colNueva - 1;
+            break;
+        }
+
+        if (esNueva) {
+            escribirFicha(tablero, indNuevo, 0); // celda nueva: queda vacia, la llena rellenarVacios despues
+        } else {
+            unsigned short indiceViejo = calcularIndice(filVieja, columnasViejas, colVieja);
+            unsigned char ficha = leerFicha(tablero, indiceViejo);
+            escribirFicha(tablero, indNuevo, ficha);
+        }
+    }
 }
--- Desafio_1/estructura.h
+++ Desafio_1/estructura.h
@@ -7,22 +7,31 @@ enum operacion {
     agregarColumna,
     borrarColumna
 };
-// Redimensiona el tablero en memoria dinámica según la operación estructural realizada (agregar/eliminar fila o columna).
-// Reubica las fichas existentes ajustando sus coordenadas, libera la memoria del tablero anterior y asigna el nuevo bloque.
+// Redimensiona el tablero en memoria dinámica según la operación realizada (agregar/eliminar fila o columna)
 void copiarConNuevasDimensiones(unsigned char *& tablero, unsigned short filasViejas, unsigned short columnasViejas, unsigned short filasNuevas,
-                                unsigned short columnasNuevas, unsigned short filaAfectada, unsigned short columnaAfectada,short tipoOperacion);
+                                unsigned short columnasNuevas, unsigned short filaAfectada, unsigned short columnaAfectada, operacion tipoOperacion);
 
 //Reorganiza y desplaza las fichas dentro del mismo bloque de memoria existente sin destruirlo ni pedir memoria nueva
 void desplazarEliminadas(unsigned char * tablero, unsigned short filasViejas, unsigned short columnasViejas,unsigned short filasNuevas,
                          unsigned short columnasNuevas, unsigned short filaAfectada, unsigned short columnaAfectada, short tipoOperacion) ;
 
-void insertarFila(unsigned char *& tablero, unsigned short & filas, unsigned short columnas, unsigned short filaAfectada);
+void insertarFila(unsigned char *& tablero, unsigned short & filas, unsigned short columnas,
+                  unsigned short filaAfectada, unsigned short & bytesReservados);
 
 void eliminarFila(unsigned char *& tablero, unsigned short & filas, unsigned short columnas, unsigned short filaAfectada,unsigned short & bytesReservados);
 
-void insertarColumna(unsigned char *& tablero, unsigned short filas, unsigned short & columnas, unsigned short columnaAfectada);
+void insertarColumna(unsigned char *& tablero, unsigned short filas, unsigned short & columnas,
+                     unsigned short columnaAfectada, unsigned short & bytesReservados);
 
 void eliminarColumna(unsigned char *& tablero, unsigned short filas, unsigned short & columnas,unsigned short columnaAfectada, unsigned short & bytesReservados);
 
 bool requiereEncogimiento(unsigned short bytesNecesarios, unsigned short bytesReservados);
+
+// Abre espacio para una fila o columna nueva dentro del mismo bloque de memoria ya reservado,
+// desplazando las fichas existentes (de atras hacia adelante, para no sobrescribir datos
+// que aun no se han leido), y deja en 0 las celdas de la fila/columna insertadas
+void desplazarInsertados(unsigned char * tablero, unsigned short filasViejas, unsigned short columnasViejas,
+                         unsigned short filasNuevas, unsigned short columnasNuevas,
+                         unsigned short filaAfectada, unsigned short columnaAfectada, short tipoOperacion);
+
 #endif // ESTRUCTURA_H
